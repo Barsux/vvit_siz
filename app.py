12f9583f012ae494a6ca14c70d4550a4dec58626
Barsux <yity.r7@gmail.com>
2022-05-20 17:42:09 +0300

    Ещё раз вроде бы выключил

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from app import app, routes
 #from app.track import detect_by_filename
 
 if __name__ == "__main__":
-    #app.run(host="0.0.0.0")
-    detect_by_filename("test2")
+    app.run(host="0.0.0.0")
+    #detect_by_filename("test2")
