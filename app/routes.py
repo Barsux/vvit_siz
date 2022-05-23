d6e6ee092e1efbaac66c40ab28b15ddfdc461cf4
Barsux <yity.r7@gmail.com>
2022-05-23 20:13:13 +0300

    Добавлен фронт

--- app/routes.py
+++ app/routes.py
@@ -4,16 +4,8 @@ from flask import render_template, flash, request
 from app.file_mgmt import trackfiles, UploadedFile
 
 @app.route("/", methods = ["GET", "POST"])
-def rootroute():
-    return render_template("root.html")
-
-@app.route("/track", methods = ["GET", "POST"])
-def track():
+def upload():
     if request.method == 'POST':
-        f = request.files['upfile']
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
-        trackfiles.append(UploadedFile(f.filename))
-        #while not is_tracked(f.filename):
-        #    pass
-        #Как-то нужно вернуть эту хуйню назад
-    return render_template("track.html")
+        f = request.files.get('file')
+        f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
+    return render_template('index.html')
