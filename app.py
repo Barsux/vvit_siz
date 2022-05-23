11e98a58a1c2860ef6cc6665fbfaa4b8676f5ed4
Barsux <yity.r7@gmail.com>
2022-05-23 20:12:39 +0300

    Добавлен другой детектор, наконец-то приделан фронт

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from app import app, routes
-#from app.track import detect_by_filename
+from app import app, routes, UPLOAD_FOLDER
+from yolov5.detect_track import run
+
+FILENAME_TO_DETECT = "test.mp4"
+WEIGHTS = "best.pt"
+SHOW_IMG = True
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
-    #detect_by_filename("test2")
+    #app.run(host="0.0.0.0")
+    run(weights=WEIGHTS, source=f"{UPLOAD_FOLDER}/{FILENAME_TO_DETECT}", view_img=SHOW_IMG)
